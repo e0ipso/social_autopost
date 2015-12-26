2d51a48619e743a604e65ce6317f5df6761cb284
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-12-26 08:50:45 +0100

    [FEATURE] Allow adding destinations for Facebook

--- modules/fb_autopost/src/Form/FacebookSettingsForm.php
+++ modules/fb_autopost/src/Form/FacebookSettingsForm.php
@@ -36,18 +36,28 @@ class FacebookSettingsForm extends ConfigFormBase implements FormInterface {
       '#title' => $this->t('Application ID'),
       '#description' => $this->t('The application ID as retrieved from the Facebook APP Dashboard.'),
       '#default_value' => $config->get('app_id') ?: '',
+      '#required' => TRUE,
     ];
     $form['app_secret'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Application Secret'),
       '#description' => $this->t('The application secret as retrieved from the Facebook APP Dashboard.'),
       '#default_value' => $config->get('app_secret') ?: '',
+      '#required' => TRUE,
     ];
     $form['version'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Graph Version'),
       '#description' => $this->t('The default graph version to use. It can be overridden per request.'),
       '#default_value' => $config->get('version') ?: '',
+      '#required' => TRUE,
+    ];
+
+    $form['destinations'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Available destinations'),
+      '#description' => $this->t("Enter a destination per line. Each line should contain the label of the destination and the Facebook's feed ID. Add the special feed ID 'me' to post to the currently visitor's Facebook feed."),
+      '#default_value' => $this::encodeDestinations($config->get('destinations')),
     ];
     return $form;
   }
@@ -68,8 +78,59 @@ class FacebookSettingsForm extends ConfigFormBase implements FormInterface {
     array_walk($properties, function ($property) use ($config, $form_state) {
       $config->set($property, $form_state->getValue($property));
     });
+
+    $destinations = $form_state->getValue('destinations');
+    $config->set('destinations', $this::parseDestinations($destinations));
+
     $config->save();
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Parses text form destinations.
+   *
+   * @param string $destinations
+   *   The pipe separated destinations configuration.
+   *
+   * @return array
+   *   The structured configuration.
+   */
+  protected static function parseDestinations($destinations) {
+    // Decompose the destinations into structured data.
+    $stuctured = [];
+    $destinations = explode("\n", $destinations);
+    foreach ($destinations as $item) {
+      // Remove possible extra white spaces.
+      $item = trim($item);
+      if (empty($item)) {
+        continue;
+      }
+      $parts = explode('|', $item);
+      if (count($parts) == 1) {
+        $parts[] = $parts[0];
+      }
+      $stuctured[$parts[1]] = [
+        'label' => $parts[0],
+        'feed_id' => $parts[1],
+      ];
+    }
+
+    return $stuctured;
+  }
+
+  /**
+   * Transform the structured config into something for a text area.
+   *
+   * @param array $destinations
+   *   The structured configuration.
+   *
+   * @return string
+   *   The configuration to be used in a text area.
+   */
+  protected static function encodeDestinations(array $destinations) {
+    return implode("\n", array_map(function ($item) {
+      return $item['label'] . '|' . $item['feed_id'];
+    }, $destinations));
   }
 
 }
